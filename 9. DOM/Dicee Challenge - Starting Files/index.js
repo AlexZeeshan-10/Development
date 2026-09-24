b1855e81cc7d258fb1decf964f0839cef7ec8d1f
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-24 17:09:31 +0530

    dice game done

---
+++ 9. DOM/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,18 @@
+var image1 = document.querySelectorAll(".img1")[0];
+image1.setAttribute("src", "./images/dice" + (Math.floor(Math.random() * 6) + 1) + ".png");
+
+
+var image2 = document.querySelectorAll(".img2")[0];
+image2.setAttribute("src", "./images/dice" + (Math.floor(Math.random() * 6) + 1) + ".png");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = " PLayer 1 Wins...!!!";
+}
+
+else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = " It's a TIE";
+}
+
+else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins...!!!";
+}
