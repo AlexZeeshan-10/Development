080d7abd20a93057b6adae71cf7aade318ea2462
AlexZeeshan_10 <180771652+AlexZeeshan-10@users.noreply.github.com>
2026-09-25 22:18:32 +0530

    node lecture 4 completed

--- 11. NodeJS/2.3 NPM/index.js
+++ 11. NodeJS/2.3 NPM/index.js
@@ -1,12 +1,3 @@
-// var generateName = require('sillyname');
-
-// import generateName from "sillyName";
-// var sillyName = generateName();
-
-// console.log(`My name is ${sillyName}.`);
-
-
-
 import { randomSuperhero } from "superheroes";
 const name = randomSuperhero();
 
